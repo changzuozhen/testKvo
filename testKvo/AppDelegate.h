a0d113e1edf7c7b9d090380137183e95b17169c2
changzuozhen <WilliamChang@ChangZuozhendeMacBook-Pro.local>
2013-12-30 16:20:42 +0800

    Initial Commit

---
+++ testKvo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  testKvo
+//
+//  Created by ChangZuozhen on 13-12-30.
+//  Copyright (c) 2013年 ChangZuozhen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ testKvo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  testKvo
+//
+//  Created by ChangZuozhen on 13-12-30.
+//  Copyright (c) 2013年 ChangZuozhen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
